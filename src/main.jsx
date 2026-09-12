087e79833e17cda017eedc290f521757c5ec4e4d
dayana <dayanaortguz123@gmail.com>
2026-09-12 19:59:18 -0400

    Feat: add new stile for home

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import Navbar from './Components/Navbar/Navbar'
-import Home from './Home/home'
+import Home from './Home/Home'
 
 
 
