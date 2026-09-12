426208b6c908f8adde7956b5f6bacc472fd8b4a2
dayana <dayanaortguz123@gmail.com>
2026-09-12 19:21:50 -0400

    feat: add Navbar and Home components with styling
    - Implemented Navbar component with responsive design and user profile section.
    - Created Home component featuring a hero section with text and buttons.
    - Added CSS styles for Navbar and Home components to enhance UI.
    - Included responsive design adjustments for various screen sizes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
-import Navbar from './Components/Navbar.jsx'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Home/home'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
